be5799c4f48b8b77a51e436a4da28cc91a554660
pratikupacharya <pratikupacharya@gmail.com>
2021-03-20 15:59:35 +0530

    Added BinaryHeapArrays.java

---
+++ Coding-Hub/src/codebook/dataStructures/heap/BinaryHeapArrays.java
@@ -0,0 +1,85 @@
+package codebook.dataStructures.heap;
+
+import java.util.Arrays;
+
+public class BinaryHeapArrays {
+ 
+	private void maxHeapify(int[] heapArray, int index, int heapSize){
+		int largest = index;
+		while (largest < heapSize / 2){      // check parent nodes only     
+			int left = (2 * index) + 1;       //left child
+			int right = (2 * index) + 2;      //right child
+      
+			if (left < heapSize && heapArray[left] > heapArray[index]){
+				largest = left; 			
+      		}
+			if (right < heapSize && heapArray[right] > heapArray[largest]){
+				largest = right;
+			}
+			if (largest != index){ // swap parent with largest child 
+				int temp = heapArray[index];
+				heapArray[index] = heapArray[largest];
+				heapArray[largest] = temp;
+				index = largest;
+			}
+      		else 
+				break; // if heap property is satisfied 
+		} //end of while
+	} 
+	public void buildMaxHeap(int[] heapArray, int heapSize) 
+  	{
+   	// swap largest child to parent node 
+		for (int i = (heapSize - 1) / 2; i >= 0; i--){
+			maxHeapify(heapArray, i, heapSize);
+		}
+	}
+
+    private void minHeapify(int[] heapArray, int index, int heapSize) {
+		int smallest = index;
+
+		while (smallest < heapSize / 2) { // check parent nodes only
+			int left = (2 * index) + 1; //left child
+			int right = (2 * index) + 2; //right child
+			if (left < heapSize && heapArray[left] < heapArray[index]) {
+				smallest = left;
+			}
+
+			if (right < heapSize && heapArray[right] < heapArray[smallest]) {
+				smallest = right;
+			}
+
+			if (smallest != index) { // swap parent with smallest child
+				int temp = heapArray[index];
+				heapArray[index] = heapArray[smallest];
+				heapArray[smallest] = temp;
+				index = smallest;
+			} else {
+				break; // if heap property is satisfied
+			}
+
+		} //end of while
+	}
+ 
+	public void buildMinHeap(int[] heapArray, int heapSize) {
+    
+		// swap smallest child to parent node 
+		for (int i = (heapSize - 1) / 2; i >= 0; i--) {
+			minHeapify(heapArray, i, heapSize);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] heapArray = { 1, 4, 7, 12, 15, 14, 9, 2, 3, 16 };
+		
+		System.out.println("Before heapify: "+Arrays.toString(heapArray));		
+		new BinaryHeapArrays().buildMaxHeap(heapArray, heapArray.length);
+		System.out.println("After heapify: "+Arrays.toString(heapArray));		
+
+
+        // Min Heap -  Uncomment the code.
+        // int[] heapArray = { 31, 11, 7, 12, 15, 14, 9, 2, 3, 16 };
+		// System.out.println("Before heapify: "+Arrays.toString(heapArray));		
+		// new BinaryHeapArrays().buildMinHeap(heapArray, heapArray.length);
+		// System.out.println("After heapify: "+Arrays.toString(heapArray));
+	}
+}
